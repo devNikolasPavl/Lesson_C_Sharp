83cd8cbd4e1c0908cb6b8db4bcbe7b004ea1074e
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-16 20:32:22 +0300

    Video_01, Пример 02

---
+++ Video_01/Ch_01/Lesson_02/Lesson_02/Lesson_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Структура проекта
+// Структура и порядок выполнения программ
+
+namespace Lesson_02
+{
+	class Programm
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Привет мир!");
+		}
+	}
+}
