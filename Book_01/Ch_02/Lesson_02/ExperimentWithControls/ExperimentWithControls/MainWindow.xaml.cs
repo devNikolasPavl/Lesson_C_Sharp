09b64cc5eb31d40aa1b61be2c1df996fadea9c6d
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-20 10:52:48 +0300

    Update MainWindow.xaml.cs

--- Book_01/Ch_02/Lesson_02/ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
+++ Book_01/Ch_02/Lesson_02/ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
@@ -11,9 +11,6 @@ using System.Windows.Shapes;
 
 namespace ExperimentWithControls
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
